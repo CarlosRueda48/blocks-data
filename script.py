5240e01e2e00a6d874580f0d885737f69420731f
CarlosRueda <carlosruedag48@gmail.com>
2021-09-23 16:00:28 -0700

    Format and add bigquery insert

--- script.py
+++ script.py
@@ -1,8 +1,16 @@
 import psycopg2
 import psycopg2.extras
 from google.cloud import bigquery
+from google.cloud import storage
+import json
+import datetime
 
 import configparser
+
+
+def datetime_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -18,31 +26,45 @@ pqlconn = psycopg2.connect(dbname=config['DEFAULT']['db_name'],
                         )
 
 # Construct a BigQuery client object.
-bqclient = bigquery.Client.from_service_account_json('keys/gcp_key.json')
 
 cursor = pqlconn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-cursor.execute("""SELECT * FROM onearizona.shifts
-ORDER BY id ASC """)
-shifts = cursor.fetchall()
-for row in shifts:
-    print(row['id'])
+
+t_path_n_file = config['DEFAULT']['shifts_create_csv_path']
+query = "copy  (select * from onearizona.shifts) TO STDOUT WITH (FORMAT csv, DELIMITER ',', HEADER)"
+with open(t_path_n_file, 'w') as f_output:
+    
+    cursor.copy_expert(query, f_output)
 
 
+storage_client = storage.Client.from_service_account_json(config['DEFAULT']['gcp_key_location'])
+bucket = storage_client.get_bucket('oneaz_blocks_data')
 
+#Upload shifts csv to Google Cloud Storage
+blob = bucket.blob('blocks_shifts.csv')
+blob.upload_from_filename('shifts.csv')
 
-query = """
-    SELECT name, SUM(number) as total_people
-    FROM `bigquery-public-data.usa_names.usa_1910_2013`
-    WHERE state = 'TX'
-    GROUP BY name, state
-    ORDER BY total_people DESC
-    LIMIT 20
-"""
-query_job = bqclient.query(query)  # Make an API request.
+# Next, we populate shifts table from csv in GCS
 
-print("The query data:")
-for row in query_job:
-    # Row values can be accessed by field name or index.
-    print("name={}, count={}".format(row[0], row["total_people"]))
+# Construct a BigQuery client object.
+bqclient = bigquery.Client.from_service_account_json(
+    config['DEFAULT']['gcp_key_location'])
 
+# Specify table id
+table_id = config['DEFAULT']['shifts_table_id']
 
+job_config = bigquery.LoadJobConfig(
+    autodetect=True,
+    skip_leading_rows=1,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+uri = config['DEFAULT']['cloud_storage_shifts_path']
+
+load_job = bqclient.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
+
+load_job.result()  # Waits for the job to complete.
+
+destination_table = bqclient.get_table(table_id)  # Make an API request.
+print("Loaded {} rows.".format(destination_table.num_rows))
