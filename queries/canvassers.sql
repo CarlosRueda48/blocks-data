e90f5fb892db202543324eb221fd15d664467183
CarlosRueda <carlosruedag48@gmail.com>
2021-09-28 13:20:35 -0700

    Add canvassers processing

---
+++ queries/canvassers.sql
@@ -0,0 +1,17 @@
+SELECT 
+	id,
+	first_name,
+	last_name,
+	phone_number,
+	notes,
+	turf_id,
+	created_at,
+	updated_at,
+	created_by_user_id,
+	organization_id,
+	address,
+	county,
+	email,
+	person_id,
+	slug
+FROM onearizona.canvassers
