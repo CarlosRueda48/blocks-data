caae8d313245ce262b67a0e421634ffd31fbb66b
CarlosRueda <carlosruedag48@gmail.com>
2022-02-03 11:43:59 -0700

    Add new quality_control_flags table for processing

---
+++ queries/quality_control_flags.sql
@@ -0,0 +1,12 @@
+SELECT 
+    id,
+    packet_id,
+    canvasser_id,
+    status,
+    created_at,
+    updated_at,
+    trigger_id,
+    triggered_by_canvasser_id,
+    completed_at,
+    triggered_by_shift_id
+FROM onearizona.quality_control_flags
